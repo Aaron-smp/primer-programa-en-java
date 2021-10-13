bc764c3e7a8f59702bee61e189ad64d1a865c0c0
Aarón Pérez Carreño <aapecargame@gmail.com>
2021-10-13 12:01:21 +0200

    Ahora dice a mayores lugar y año de nacimiento

--- Prueba.java
+++ Prueba.java
@@ -3,6 +3,8 @@ public class Prueba {
 		public static void main(String args[]) {
 		
 		System.out.println("Aarón Pérez Carreño");
+		System.out.println("Laguna dalga");
+		System.out.println("2002");
 
 		}
 
