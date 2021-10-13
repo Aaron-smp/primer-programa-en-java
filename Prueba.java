b5055eb67aaa3a2aed6eb700a6b3b130b13ef53b
Aarón Pérez Carreño <aapecargame@gmail.com>
2021-10-13 12:04:00 +0200

    Residencia y SO actual

--- Prueba.java
+++ Prueba.java
@@ -3,8 +3,8 @@ public class Prueba {
 		public static void main(String args[]) {
 		
 		System.out.println("Aarón Pérez Carreño");
-		System.out.println("Laguna dalga");
-		System.out.println("2002");
+		System.out.println("Laguna dalga 2002");
+		System.out.println("Santa María del Páramo y uso windows 10");
 
 		}
 
