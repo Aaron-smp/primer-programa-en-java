b8fe9c61fe1418278e99f032718d84286cfa189d
Aarón Pérez Carreño <aapecargame@gmail.com>
2021-10-15 11:15:25 +0200

    Cantante favorito

--- Prueba.java
+++ Prueba.java
@@ -2,10 +2,10 @@ public class Prueba {
 		
 		public static void main(String args[]) {
 		
-		System.out.println("Aarón Pérez Carreño");
+		System.out.println("Pop Smoke");
 		System.out.println("Laguna dalga 2002");
 		System.out.println("Santa María del Páramo y uso windows 10");
 
 		}
 
-}
+} 
